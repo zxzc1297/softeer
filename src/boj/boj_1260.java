fbcbf004ce45d68e630c0df5e9533fffeaf2835f
Lee <zxzc9712@naver.com>
2024-07-25 19:59:25 +0900

    feat: boj dfs와 bfs

---
+++ src/boj/boj_1260.java
@@ -0,0 +1,99 @@
+package boj;
+import java.util.*;
+import java.io.*;
+
+/**
+ * 문제
+ * 그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+ *
+ * 입력
+ * 첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
+ *
+ * 출력
+ * 첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
+ *
+ * 예제 입력 1
+ * 4 5 1
+ * 1 2
+ * 1 3
+ * 1 4
+ * 2 4
+ * 3 4
+ * 예제 출력 1
+ * 1 2 4 3
+ * 1 2 3 4
+ * 예제 입력 2
+ * 5 5 3
+ * 5 4
+ * 5 2
+ * 1 2
+ * 3 4
+ * 3 1
+ * 예제 출력 2
+ * 3 1 2 5 4
+ * 3 1 4 2 5
+ * 예제 입력 3
+ * 1000 1 1000
+ * 999 1000
+ * 예제 출력 3
+ * 1000 999
+ * 1000 999
+ */
+public class boj_1260 {
+    static int[][] maps;
+    static boolean[] visited;
+    static int N,M,start;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+
+        maps = new int[N+1][N+1];
+        visited = new boolean[N+1];
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            maps[x][y] = maps[y][x] = 1;
+        }
+
+        dfs(start);
+        System.out.println();
+        bfs();
+    }
+
+    static void dfs(int idx){
+        System.out.print(idx+ " ");
+        visited[idx] = true;
+
+        for(int i=1; i<=N; i++){
+            if(!visited[i] && maps[idx][i] == 1){
+                dfs(i);
+            }
+        }
+    }
+
+    static void bfs(){
+        Queue<Integer> q = new LinkedList<>();
+
+        visited = new boolean[N+1];
+        q.offer(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+            System.out.print(now + " ");
+
+            for(int i=1; i<=N; i++){
+                if(!visited[i] && maps[now][i] == 1){
+                    visited[i] = true;
+                    q.offer(i);
+                }
+            }
+        }
+    }
+}
